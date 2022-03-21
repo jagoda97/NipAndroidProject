f8cb03bf620e5d7ce8f08caeed887b2307fcd0a2
Jagoda <mamdosc48@gmail.com>
2022-03-22 00:45:15 +0100

    added EditText and function to generate the date

--- app/src/main/java/com/example/volley2/MainActivity.java
+++ app/src/main/java/com/example/volley2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Cache;
@@ -19,27 +20,43 @@ import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.google.gson.Gson;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
 
+    EditText text;
+    String generatedDate=setDate();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn=findViewById(R.id.button);
-        String url = "https://wl-api.mf.gov.pl/api/search/nip/5260250274?date=2021-12-05";
-                //"https://programming-quotes-api.herokuapp.com/quotes/random";
+        btn.setText("Szukaj");
+        String nip;
+       // nip=String.valueOf(text.getText());
+        //526025027452
+        text=findViewById(R.id.editText);
+        text.setHint("Wprowadz nip");
+        //"https://programming-quotes-api.herokuapp.com/quotes/random";
+        setDate();
         TextView textView= findViewById(R.id.textView);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    volleyWork(textView,url);
+                //9270101431
+                String url = "https://wl-api.mf.gov.pl/api/search/nip/"+text.getText()+generatedDate;
+                        //"?date=2021-12-05";
+                volleyWork(textView,url);
 
             }
         });
 
     }
+
+
 
     public void volleyWork(TextView t,String url)
     {
@@ -76,6 +93,15 @@ public class MainActivity extends AppCompatActivity {
                 });
         requestQueue.add(stringRequest);
 
+    }
+
+    public String setDate()
+    {
+        Date date=new Date();
+        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+        // System.out.println(formatDate.format(date));
+        String stringDate=formatDate.format(date);
+        return "?date="+stringDate;
     }
 
 }
